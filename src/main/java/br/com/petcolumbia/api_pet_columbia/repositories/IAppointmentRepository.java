2b9487fb3f73a14491b3a618c26cab86f272b9e6
SamuelPazz <samuel.paz@sptech.school>
2025-05-18 20:43:03 -0300

    inserts sql, initial dashboard

--- src/main/java/br/com/petcolumbia/api_pet_columbia/repositories/IAppointmentRepository.java
+++ src/main/java/br/com/petcolumbia/api_pet_columbia/repositories/IAppointmentRepository.java
@@ -2,7 +2,10 @@ package br.com.petcolumbia.api_pet_columbia.repositories;
 
 import br.com.petcolumbia.api_pet_columbia.domain.entities.AppointmentModel;
 import br.com.petcolumbia.api_pet_columbia.domain.entities.EmployeeModel;
+import br.com.petcolumbia.api_pet_columbia.dtos.responses.appointmentDtos.AppointmentCountDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -14,4 +17,14 @@ public interface IAppointmentRepository extends JpaRepository<AppointmentModel, 
             EmployeeModel employee, LocalDateTime startOfDay, LocalDateTime endOfDay);
 
     List<AppointmentModel> findByStartDateTimeBetween(LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT new br.com.petcolumbia.api_pet_columbia.dtos.responses.appointmentDtos.AppointmentCountDto(DATE(a.startDateTime), COUNT(a)) " +
+            "FROM AppointmentModel a " +
+            "WHERE a.startDateTime BETWEEN :start AND :end " +
+            "GROUP BY DATE(a.startDateTime) " +
+            "ORDER BY DATE(a.startDateTime) ASC")
+    List<AppointmentCountDto> countAppointmentsGroupedByDay(@Param("start") LocalDateTime start,
+                                                            @Param("end") LocalDateTime end);
+
+
 }
