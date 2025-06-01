2a1fa80efaad55b65e8b4a79430b385bc26b463d
SamuelPazz <samuel.paz@sptech.school>
2025-06-01 17:27:52 -0300

    add method and fix sql

--- src/main/java/br/com/petcolumbia/api_pet_columbia/controllers/ServicesController.java
+++ src/main/java/br/com/petcolumbia/api_pet_columbia/controllers/ServicesController.java
@@ -3,14 +3,16 @@ package br.com.petcolumbia.api_pet_columbia.controllers;
 import br.com.petcolumbia.api_pet_columbia.domain.entities.ServiceModel;
 import br.com.petcolumbia.api_pet_columbia.dtos.mappers.ServiceMapper;
 import br.com.petcolumbia.api_pet_columbia.dtos.responses.serviceDtos.ServiceResponseDto;
+import br.com.petcolumbia.api_pet_columbia.dtos.responses.serviceDtos.ServicesWithPriceResponseDto;
+import br.com.petcolumbia.api_pet_columbia.services.ServicePriceAndDurationService;
 import br.com.petcolumbia.api_pet_columbia.services.ServiceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Dictionary;
 import java.util.List;
 
 @RestController
@@ -18,9 +20,11 @@ import java.util.List;
 public class ServicesController {
 
     private final ServiceService serviceService;
+    private final ServicePriceAndDurationService priceAndDurationService;
 
-    public ServicesController(ServiceService serviceService) {
+    public ServicesController(ServiceService serviceService, ServicePriceAndDurationService priceAndDurationService) {
         this.serviceService = serviceService;
+        this.priceAndDurationService = priceAndDurationService;
     }
 
     @GetMapping()
@@ -29,5 +33,16 @@ public class ServicesController {
     public ResponseEntity<List<ServiceResponseDto>> getServices(){
         List<ServiceModel> servicesList = serviceService.listServices();
         return ResponseEntity.status(200).body(ServiceMapper.entitiesToResponses(servicesList));
+    }
+
+    @GetMapping("/price/{petId}")
+    @Operation(summary = "Busca os preços de todos os serviços para um pet específico", description = "Recebe o id do pet e uma lista de ids dos serviços")
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<List<ServicesWithPriceResponseDto>> getServicesPricesOfPet(@PathVariable Integer petId, @RequestParam List<Integer> servicesIds) {
+        List<ServicesWithPriceResponseDto> servicesList = priceAndDurationService.servicesPricesByPetAndServicesIds(petId, servicesIds);
+        if (servicesList.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(servicesList);
     }
 }
---
+++ src/main/java/br/com/petcolumbia/api_pet_columbia/dtos/responses/serviceDtos/ServicesWithPriceResponseDto.java
@@ -0,0 +1,40 @@
+package br.com.petcolumbia.api_pet_columbia.dtos.responses.serviceDtos;
+
+public class ServicesWithPriceResponseDto {
+    private Integer id;
+    private String serviceName;
+    private Double price;
+
+    public ServicesWithPriceResponseDto() {
+    }
+
+    public ServicesWithPriceResponseDto(Integer id, String serviceName, Double price) {
+        this.id = id;
+        this.serviceName = serviceName;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
