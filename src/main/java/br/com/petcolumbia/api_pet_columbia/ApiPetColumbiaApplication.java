d120c793bc4ff539bd69274b00757b71509abef4
SamuelPazz <samuel.paz@sptech.school>
2025-12-04 19:31:38 -0300

    teste ci-cd

--- src/main/java/br/com/petcolumbia/api_pet_columbia/ApiPetColumbiaApplication.java
+++ src/main/java/br/com/petcolumbia/api_pet_columbia/ApiPetColumbiaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiPetColumbiaApplication {
 	//url test: http://localhost:8080/swagger-ui/index.html
+	//test ci-cd
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPetColumbiaApplication.class, args);
 	}
